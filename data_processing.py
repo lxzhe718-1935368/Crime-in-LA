43f9634020cc7fcc9ec385ab9cfcccbcac80c039
Jack Liu <lxzhe718@uw.edu>
2022-03-08 00:46:24 -0800

    finish all comment

--- data_processing.py
+++ data_processing.py
@@ -1,3 +1,11 @@
+"""
+Xiaozhe(Jack) Liu, Jieyun(Ellie) Xie
+2022/3/08
+This file is a part of implemention of cse 163 final project.
+This file is data processing part, do not have main()
+provide method that process la crime and la map data
+in order to let final_project.py can use them to make data analysis.
+"""
 import geopandas as gpd
 import pandas as pd
 from shapely.geometry import Point
@@ -7,10 +15,11 @@ LA_FILE_PATH = "C:/Users/718/Desktop/cse163/LAPD_map/LAPD_Divisions.shp"
 
 def get_LAcrime_geodata(la_map):
     """
-    process the la crime data and return as geodata set,
-    that we can use in all the part of project.
+    process original la crime data and return as geodata set,
+    took la_map to make sure their .crs location is same.
+    Use return data in all the part of project.
     Ignore all the row that lat is 0.0 (did not provide location)
-    SOmetimes, we will use this data as dataFrame instead of geoDataFrame
+    Sometimes, we will use this data as dataFrame instead of geoDataFrame
     """
     la_crime_data = pd.read_csv(Crime_FILE_PATH)
     la_crime_data = la_crime_data.loc[la_crime_data["LAT"] != 0]
@@ -27,10 +36,10 @@ def get_LAcrime_geodata(la_map):
 
 def get_LAmap():
     """
-    The area name in la_map is a little different with crime data.
     Process la map data that make area name match with
     la crime data. Return after process data.
-    We can join la map data to crime data after area name is consist.
+    Make area name is consist,
+    we can join la map data to crime data after processing.
     We use it for all the part of project.
     """
     la_map = gpd.read_file(LA_FILE_PATH)
@@ -58,17 +67,14 @@ def join_map_crime(la_crime_data, la_map):
     Helper method for crime_in_map,
     Take both la crime data and la map data,
     return geoData that join those data together with
-    count of crime. And processed data for location graph
+    count of crime, and processed data for plot top 15 crime took place.
     """
     crime_count = la_crime_data.groupby("AREA NAME", as_index=False)["count"].sum()
     # Choice any other column will work because we use count
     crime_count = crime_count[["AREA NAME", "count"]]
     # put la_map in left will keep data in geoData
     # I try to use sjoint with dissolve(instead of groupby)
-    # but it took so long to run the dissolve funciton, here is the code:
-    # crime_count = gpd.sjoin(la_map, la_crime_data, how='inner', op='intersects')
-    # crime_count = crime_count[["APREC", "count", "geometry"]]
-    # crime_count = crime_count.dissolve(by="APREC", aggfunc='sum')
+    # but it took so long to run the dissolve funciton
     # so I come up this alternate way to process data
     location_count = la_crime_data.groupby("Premis Desc",
                                            as_index=False)["count"].sum()
@@ -85,7 +91,7 @@ def crime_type_data(la_crime_data):
     Helper method for crime_type_analysis()
     Takes la crime data return dataFrame that use to plot the
     relationship. The size > 3000 simply control Premises shows in y axis.
-    because it filter out premises that have low count.
+    because it filter out premises that have low crime count.
     """
     # select top happend crime type
     check = la_crime_data.groupby("Crm Cd Desc", as_index=False)["count"].sum()
@@ -99,13 +105,23 @@ def crime_type_data(la_crime_data):
 
 
 def wapon_data_process(la_crime_data):
-    # limited data to top 10 wapon that invoded in crime
+    """
+    Helper method for wapon_used()
+    Takes la crime data and return dataFrame that contains
+    top 10 wapon that involved in crime
+    """
     wapon_data = la_crime_data[la_crime_data["Weapon Used Cd"] != 500.0]
     wapon_data = wapon_data.groupby("Weapon Desc", as_index=False)["count"].sum()
     wapon_data.sort_values(by=["count"], ascending=False, inplace=True)
     return wapon_data.head(n=10)
 
 def wapon_map_process(la_crime_data, la_map):
+    """
+    Helper method for wapon_in_map()
+    Takes la crime and la map data and
+    return geodataFrame that contains processed data about
+    crime that involve strong arm and crime that involve pistol
+    """
     # data involved strong arm
     wapon_strong_arm = la_crime_data[la_crime_data["Weapon Desc"] ==
         "STRONG-ARM (HANDS, FIST, FEET OR BODILY FORCE)"]
@@ -123,6 +139,11 @@ def wapon_map_process(la_crime_data, la_map):
 
 
 def victims_data(la_crime_data):
+    """
+    Helper method for victims_analysis()
+    Takes la crime and return filtered geoData contains valid information
+    about victims's sex and age. Only use it as dataFrame.
+    """
     victims = la_crime_data[(la_crime_data["Vict Sex"] != "X") &
                             (la_crime_data["Vict Age"] > 0) &
                             (la_crime_data["Vict Sex"] != "H")]
